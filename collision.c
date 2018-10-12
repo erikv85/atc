b7b9ddb63a9184cec7fa89cef8e67827bf845aa5
Erik Vesterlund <erik.vesterlund@gmail.com>
2018-10-12 15:00:01 -0400

    fix to kernel style

--- collision.c
+++ collision.c
@@ -15,9 +15,7 @@ float *solve_quadratic(float a, float b, float c)
 	float discr = b * b - 4 * a * c;
 	if (denom == 0 || discr < 0) { // NaN or non-real roots
 		return NULL;
-	}
-	else
-{
+	} else {
 		float *x = malloc(2 * sizeof(float));
 		*x = (-b + sqrt(discr))/denom;
 		*(x + 1) = (-b - sqrt(discr))/denom;
