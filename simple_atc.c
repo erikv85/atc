462ddedcc9813369affaad00ab91dc67d155eb5c
Erik Vesterlund <erik.vesterlund@gmail.com>
2018-10-14 14:51:13 -0400

    use aircrafts and not disks, and play around with two airplanes

--- simple_atc.c
+++ simple_atc.c
@@ -4,8 +4,8 @@
 #include <fcntl.h>
 #include <string.h> /* strcmp, strlen */
 
+#include "aircraft.h"
 #include "graphics.h"
-#include "collision.h"
 
 void test2d(char **argv);
 
@@ -14,10 +14,6 @@ int main(int argc, char **argv)
         int graphics = 1; /* graphics is on by default */
         if (argc > 1) {
                 graphics = atoi(argv[1]);
-                if (graphics == 2) {
-                        test2d(argv);
-                        return 0;
-                }
         }
 
         /* set up params that radar and ctrl both need */
@@ -51,22 +47,29 @@ int main(int argc, char **argv)
                 close(rad2ctrl[READ_END]);
                 char ctrl_msg[10] = { '\0' };
 
-                struct disk *d = new_disk(0, 0.5, 0.025, 0.1, 0);
+                struct aircraft *acs[2];
+                acs[0] = new_aircraft(0, 0.5, 0.025, 0.1, 0);
+                set_freq(acs[0], 100);
+                set_id(acs[0], 1);
+                acs[1] = new_aircraft(1, 0.75, 0.025, -0.05, 0);
+                set_freq(acs[1], 100);
+                set_id(acs[1], 1);
                 float vnew = 0; /* radar will read new velocity into this var */
 
                 if (graphics)
                         InitializeGraphics(argv[0], 400, 400);
-                while (d->xc >= SECTOR_START && d->xc <= SECTOR_END) {
-                        move(d);
+                while (acs[0]->d->xc >= SECTOR_START && acs[0]->d->xc <= SECTOR_END) {
+                        move_aircraft(acs[0]);
+                        move_aircraft(acs[1]);
                         if (graphics) {
                                 ClearScreen();
-                                DrawCircle(d->xc, d->yc, 1, 1, d->radius, 0);
+                                DrawCircle(acs[0]->d->xc, acs[0]->d->yc, 1, 1, acs[0]->d->radius, 0);
+                                DrawCircle(acs[1]->d->xc, acs[1]->d->yc, 1, 1, acs[1]->d->radius, 0);
                                 Refresh();
                         }
                         sleep(1);
 
-                        /*
-                         * Check if controller has left a message. If he has, check if
+                        /* Check if controller has left a message. If he has, check if
                          * its format is correct. If it is correct, read the new
                          * velocity, update velocity, and send ACK to control. If the
                          * message does not have a correct format, send NACK to
@@ -83,9 +86,9 @@ int main(int argc, char **argv)
                                         write(rad2ctrl[WRITE_END], "NACK", 5);
                                 } else if (ctrl_msg[0] == 'v') {
                                         vnew = atof(ctrl_msg+2);
-                                        d->xv = vnew;
-                                        printf("Correct message format '%s', updating velocity to %f " \
-                                                "and sending ACK.\n", ctrl_msg, d->xv);
+                                        acs[0]->d->xv = vnew;
+                                        printf("Correct message format '%s', updated velocity to %f, " \
+                                                "sending ACK.\n", ctrl_msg, acs[0]->d->xv);
                                         write(rad2ctrl[WRITE_END], "ACK", 5);
                                 } else { //this should not happen
                                         fprintf(stderr, "A message was read but neither ACK nor " \
@@ -103,7 +106,8 @@ int main(int argc, char **argv)
                         CloseDisplay();
                 }
                 printf("No more traffic in sector.\n");
-                free_disk(d);
+                free_aircraft(acs[0]);
+                free_aircraft(acs[1]);
         } else { /* Control is the parent process */
                 close(ctrl2rad[READ_END]);
                 close(rad2ctrl[WRITE_END]);
@@ -127,26 +131,4 @@ int main(int argc, char **argv)
         }
 
         return 0;
-}
-
-void test2d(char **argv)
-{
-        struct disk d1 = { 0.025, 0.75, 0.025, 0.000001, -0.000001 };
-        struct disk d2 = { 0.025, 0.25, 0.025, 0.000001, 0.000001 };
-        InitializeGraphics(argv[0], 400, 400);
-        while (1) {
-                ClearScreen();
-                DrawCircle(d1.xc, d1.yc, 1, 1, d1.radius, 0);
-                DrawCircle(d2.xc, d2.yc, 1, 1, d2.radius, 0);
-                float *times = time_of_collision(d1, d2);
-                float t_min = min(times[0], times[1]);
-                if (t_min > 0)
-                        printf("Collision in %f seconds\n", t_min);
-                free(times);
-                Refresh();
-                move(&d1);
-                move(&d2);
-        }
-        FlushDisplay();
-        CloseDisplay();
 }
